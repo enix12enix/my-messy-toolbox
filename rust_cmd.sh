be8d564c7d6166226ad6d9d07492f57d0d056be5
enix12enix <enix12enix@gmail.com>
2025-07-07 15:12:56 +0800

    update

--- rust_cmd.sh
+++ rust_cmd.sh
@@ -4,3 +4,4 @@ echo "============================== rustup =============================="
 echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
 
 echo "============================== cargo ==============================="
+echo "cargo new/build/run/test project"
